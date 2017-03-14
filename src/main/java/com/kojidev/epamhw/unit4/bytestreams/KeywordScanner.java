187e91f4ec93959c86cd1c299b14ff702db3c298
stan <koji6ac9h@gmail.com>
2017-03-14 22:24:18 +0300

    Fix

--- src/main/java/com/kojidev/epamhw/unit4/bytestreams/KeywordScanner.java
+++ src/main/java/com/kojidev/epamhw/unit4/bytestreams/KeywordScanner.java
@@ -1,15 +1,17 @@
 package com.kojidev.epamhw.unit4.bytestreams;
 
-import com.sun.xml.internal.ws.util.StringUtils;
+import sun.misc.IOUtils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringJoiner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -24,7 +26,7 @@ public class KeywordScanner {
             "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch",
             "extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally",
             "long", "strictfp", "volatile", "const", "float", "native", "super", "while"
-    }) + "\\b");
+    }) + "\\b", Pattern.MULTILINE);
 
     public KeywordScanner(String s) {
         path = s;
@@ -36,10 +38,15 @@ public class KeywordScanner {
 
     private Map<String, Integer> getKeyWordsAndCount() throws IOException {
 
-        StringJoiner sj = new StringJoiner(" ");
-        Files.lines(Paths.get(path), StandardCharsets.UTF_8).forEach(sj::add);
+        StringBuilder sb = new StringBuilder();
+        FileInputStream fs = new FileInputStream(path);
+        int c;
+        while ((c = fs.read()) != -1) {
+            sb.append((char) c);
+        }
 
-        Matcher matcher = pattern.matcher(sj.toString());
+        String input = sb.toString();
+        Matcher matcher = pattern.matcher(input);
 
         HashMap<String, Integer> hm = new HashMap<>();
 
