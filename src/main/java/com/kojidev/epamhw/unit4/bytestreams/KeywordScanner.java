5909eddb8cda039614973a5ccd9abdcd77b9661c
stan <koji6ac9h@gmail.com>
2017-03-14 22:02:33 +0300

    Unir4 task 1 completed

--- src/main/java/com/kojidev/epamhw/unit4/bytestreams/KeywordScanner.java
+++ src/main/java/com/kojidev/epamhw/unit4/bytestreams/KeywordScanner.java
@@ -1,17 +1,70 @@
 package com.kojidev.epamhw.unit4.bytestreams;
 
+import com.sun.xml.internal.ws.util.StringUtils;
+
 import java.io.File;
-import java.util.Set;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class KeywordScanner {
+
+    private final String path;
+
+    private static final Pattern pattern = Pattern.compile("\\b" + String.join("|", new String[] {
+            "abstract", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "synchronized",
+            "boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw", "byte",
+            "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch",
+            "extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally",
+            "long", "strictfp", "volatile", "const", "float", "native", "super", "while"
+    }) + "\\b");
+
     public KeywordScanner(String s) {
+        path = s;
     }
 
-    public Set<String> getKeyWords() {
-        return null;
+    public Set<String> getKeyWords() throws IOException {
+        return getKeyWordsAndCount().keySet();
     }
 
-    public File dumpKeywords(String outputFile) {
-        return null;
+    private Map<String, Integer> getKeyWordsAndCount() throws IOException {
+
+        StringJoiner sj = new StringJoiner(" ");
+        Files.lines(Paths.get(path), StandardCharsets.UTF_8).forEach(sj::add);
+
+        Matcher matcher = pattern.matcher(sj.toString());
+
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        while (matcher.find()) {
+            String key = matcher.group();
+            Integer value = hm.get(key);
+            value = value == null ? 1 : value + 1;
+            hm.put(key, value);
+        }
+
+        return hm;
+    }
+
+    public void dumpKeywords(String outputFile) throws IOException {
+
+        Map<String, Integer> hm = getKeyWordsAndCount();
+
+        try(FileOutputStream fo = new FileOutputStream(outputFile)) {
+            fo.write(getTextRepresentation().getBytes());
+        }
+    }
+
+    private String getTextRepresentation() throws IOException {
+        return getKeyWordsAndCount().entrySet().stream()
+                .map(e -> e.getKey() + "\t\t" + e.getValue())
+                .collect(Collectors.joining("\n"));
     }
 }
--- src/test/java/com/kojidev/epamhw/unit4/bytesteams/KeywordScannerTest.java
+++ src/test/java/com/kojidev/epamhw/unit4/bytesteams/KeywordScannerTest.java
@@ -1,12 +1,12 @@
 package com.kojidev.epamhw.unit4.bytesteams;
 
 import com.kojidev.epamhw.unit4.bytestreams.KeywordScanner;
-import junit.framework.Assert;
 import junitx.framework.FileAssert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -18,24 +18,33 @@ public class KeywordScannerTest {
     private KeywordScanner ks;
 
     private Set<String> expectedKeywords = new HashSet<>(Arrays.asList(new String[]{
-            "import",
-            "if"
+            "implements", "private", "import", "const", "for", "do", "while", "float", "long", "default", "public",
+            "protected", "assert", "else", "continue", "catch", "if", "class", "case", "new", "package", "static",
+            "void", "break", "byte", "this", "volatile", "int", "instanceof", "boolean", "throw", "char", "final",
+            "short", "try", "return"
     }));
 
     @Before
     public void init() {
-        ks = new KeywordScanner("Scanner.java");
+        ks = new KeywordScanner("src/main/resources/Scanner.java");
     }
 
     @Test
-    public void testGetKeywords() {
+    public void testGetKeywords() throws IOException {
         Set<String> keyWords = ks.getKeyWords();
         assertEquals(keyWords, expectedKeywords);
     }
 
-    public void testDumpKeywords() {
-        File file = ks.dumpKeywords("output.txt");
-        FileAssert.assertEquals(file, new File("expected_output.txt"));
+    @Test
+    public void testDumpKeywords() throws IOException {
+
+        String path = "src/main/resources/unit4/keywords_output.txt";
+        ks.dumpKeywords(path);
+
+        FileAssert.assertEquals(
+                new File(path),
+                new File("src/main/resources/unit4/expected_keywords_output.txt")
+        );
     }
 
 
