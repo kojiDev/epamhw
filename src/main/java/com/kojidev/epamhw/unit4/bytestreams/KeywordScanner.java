22d4b3fdeca68bcf334bc27b4984961ad56c2749
stan <koji6ac9h@gmail.com>
2017-03-14 20:01:30 +0300

    Tests

---
+++ src/main/java/com/kojidev/epamhw/unit4/bytestreams/KeywordScanner.java
@@ -0,0 +1,17 @@
+package com.kojidev.epamhw.unit4.bytestreams;
+
+import java.io.File;
+import java.util.Set;
+
+public class KeywordScanner {
+    public KeywordScanner(String s) {
+    }
+
+    public Set<String> getKeyWords() {
+        return null;
+    }
+
+    public File dumpKeywords(String outputFile) {
+        return null;
+    }
+}
--- src/test/java/com/kojidev/epamhw/unit4/bytesteams/KeywordScannerTest.java
+++ src/test/java/com/kojidev/epamhw/unit4/bytesteams/KeywordScannerTest.java
@@ -1,12 +1,26 @@
 package com.kojidev.epamhw.unit4.bytesteams;
 
+import com.kojidev.epamhw.unit4.bytestreams.KeywordScanner;
+import junit.framework.Assert;
+import junitx.framework.FileAssert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
+
+import static junit.framework.Assert.assertEquals;
 
 public class KeywordScannerTest {
 
-    KeywordScanner ks;
+    private KeywordScanner ks;
+
+    private Set<String> expectedKeywords = new HashSet<>(Arrays.asList(new String[]{
+            "import",
+            "if"
+    }));
 
     @Before
     public void init() {
@@ -16,13 +30,12 @@ public class KeywordScannerTest {
     @Test
     public void testGetKeywords() {
         Set<String> keyWords = ks.getKeyWords();
-
-        //assert key words here
+        assertEquals(keyWords, expectedKeywords);
     }
 
     public void testDumpKeywords() {
-        ks.dumpKeywords("output.txt");
-        // assert here
+        File file = ks.dumpKeywords("output.txt");
+        FileAssert.assertEquals(file, new File("expected_output.txt"));
     }
 
 
